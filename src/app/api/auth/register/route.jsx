aef41fda8d3a7440c0b779f593ed43a81be20b7c
HenryBarrera95 <hsbarrerap@unal.edu.co>
2024-02-26 11:52:41 -0500

    issue: login and signup

--- src/app/api/auth/register/route.jsx
+++ src/app/api/auth/register/route.jsx
@@ -3,51 +3,54 @@ import bcrypt from "bcrypt";
 import db from "@/libs/prisma";
 
 export async function POST(request) {
-  const data = await request.json();
+  try {
+    const data = await request.json();
 
-  const emailFound = await db.user.findUnique({
-    where: {
-      email: data.email,
-    },
-  });
-
-  const usernameFound = await db.user.findUnique({
-    where: {
-      username: data.username,
-    },
-  });
-
-  if (emailFound) {
-    return NextResponse.json(
-      {
-        message: "Email already exists",
+    const emailFound = await db.user.findUnique({
+      where: {
+        email: data.email,
       },
-      {
-        status: 400,
-      }
-    );
-  }
+    });
 
-  if (usernameFound) {
-    return NextResponse.json(
-      {
-        message: "Ussername already exists",
+    const usernameFound = await db.user.findUnique({
+      where: {
+        username: data.username,
       },
-      {
-        status: 400,
-      }
-    );
+    });
+
+    if (emailFound) {
+      return NextResponse.json(
+        {
+          message: "Email already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (usernameFound) {
+      return NextResponse.json(
+        {
+          message: "Ussername already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const newUser = await db.user.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+    const { password: _, ...user } = newUser;
+    return NextResponse.json(newUser);
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
   }
-
-  console.log(data);
-  const hashedPassword = await bcrypt.hash(data.password, 10);
-  const newUser = await db.user.create({
-    data: {
-      username: data.username,
-      email: data.email,
-      password: hashedPassword,
-    },
-  });
-
-  return NextResponse.json(newUser);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 function HomePage() {
+  const router = useRouter();
   return (
-    <div>
-      <h1>The Home Page For Granjas del Carmen</h1>
+    <div className="grid-cols-2">
+      <h1 className="text-white text-4xl text-center">Granjas del Carmen</h1>
+      <section className=" mx-auto grid-cols-2 space-y-3 space-x-3">
+        <button
+          className="text-white bg-blue-950 p-3 rounded-lg mx-auto col-span-1"
+          onClick={() => router.push("/auth/login")}
+        >
+          Ingresar
+        </button>
+        <button
+          className="text-white bg-blue-950 p-3 rounded-lg mx-auto col-span-1"
+          onClick={() => router.push("/auth/register")}
+        >
+          Registrarse
+        </button>
+      </section>
     </div>
   );
 }
