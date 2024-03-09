2f9933fec06cefe85a09067e3448cbd3f6922122
HenryBarrera95 <hsbarrerap@unal.edu.co>
2024-03-09 11:12:41 -0500

    add analytics vercerl

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 interface Props {
   children: React.ReactNode;
@@ -10,6 +11,7 @@ const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div>
       <Header />
+      <Analytics />
       {children}
       <Footer />
     </div>
