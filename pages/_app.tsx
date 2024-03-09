e79cf8a71c5edb21f4b8d2445a538198fd1b3a66
HenryBarrera95 <hsbarrerap@unal.edu.co>
2024-03-09 11:46:47 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import "../styles/tailwind.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { useUser } from "@auth0/nextjs-auth0/client";
 import Layout from "../components/Layout";
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import apolloClient from "../lib/apollo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <ApolloProvider client={apolloClient}>
+        <GoogleAnalytics gaId="G-DEMQ7T7P3Q" />
         <Layout>
           <Component {...pageProps} />
         </Layout>
