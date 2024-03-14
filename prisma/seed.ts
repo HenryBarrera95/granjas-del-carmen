83bfc52d556b159c384f8de2cf57730e4b86014f
HenryBarrera95 <hsbarrerap@unal.edu.co>
2024-03-14 15:52:20 -0500

    use seeds just in dev env

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,25 +2,27 @@ import { PrismaClient, Role } from "../prisma/generated/client";
 import { rabbits } from "../data/rabbits";
 const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.user.create({
-    data: {
-      name: "test Admin",
-      email: `testemail@gmail.com`,
-      role: Role.ADMIN,
-    },
-  });
+if (process.env.NODE_ENV === "development") {
+  const main = async () => {
+    await prisma.user.create({
+      data: {
+        name: "test Admin",
+        email: `testemail@gmail.com`,
+        role: Role.ADMIN,
+      },
+    });
 
-  await prisma.rabbit.createMany({
-    data: rabbits,
-  });
+    await prisma.rabbit.createMany({
+      data: rabbits,
+    });
+  };
+
+  main()
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
 }
-
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
